8fec7f2272cf33e1c113a96e079c37bd0d6e7dd5
Taras Sheketa <taras.sheketa@gmail.com>
2023-11-13 21:19:51 +0200

    Fix issues

--- app/currency/models.py
+++ app/currency/models.py
@@ -5,5 +5,5 @@ class Rate(models.Model):
     buy = models.DecimalField(max_digits=6, decimal_places=2)
     sell = models.DecimalField(max_digits=6, decimal_places=2)
     created = models.DateTimeField()
-    currency_type = models.CharField(max_length = 3)
+    currency_type = models.CharField(max_length=3)
     source = models.CharField(max_length=255)
